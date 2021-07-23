191238aaa9dd8e1580163e2cea28f30877899743
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-07-23 21:48:55 +0545

    first file

---
+++ start.py
@@ -0,0 +1,14 @@
+#Syntax are the lines of codes which is executed.
+#eg:
+
+print("Hello WOrld")
+
+#Identation : They refer to the spaces in the beginning and they are very important in python and equal identation refers block of code.
+
+if 5>2:
+    print("a")
+else:
+    print("b")
+
+#there is equal space of both print statement and they act as same block of code.If not equal identation error wil occcur.
+    
