b9fbbdc5d3110743e541ee7c41908c4baff59bd5
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-07-25 14:46:36 +0545

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,59 @@
+#data type is important concept in programming language ans so is it in python because every input we take from user and give result to them is certain tye of data
+#variables stores data in different data types
+
+#Text type: str
+
+#Numeric type : int, float, complex
+
+#Sequence type: ;ist, tuple, range
+
+#Mapping type : dict
+
+#Set type :  set, frizenset
+
+#Boolean type : bool
+
+#Binary type : bytes, bytearray, memoryview
+
+#type() method can be used to get data type of any variable
+
+a = "Hello World" #str
+
+b = 20 #int
+
+c = 20.20 #float
+
+d = 32j #complex
+
+e = ["apple","pear","cherry","banana"] #list
+
+f = ("apple","pear","cherry","banana") #tuple
+
+g = range(6) #range
+
+h = {'name' : 'Sammy' , 'age' : 23} #dict
+
+i = {"apple","pear","cherry","banana"} #set
+
+j = ({"apple","pear","cherry","banana"}) #frozenset
+
+k = True
+l = False # bool
+
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+
+
+
