9ec8bf9f43c12ead6cb745262fbdfedcfd22a07a
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-08-14 20:47:49 +0545

    Update num_reverse.py

--- num_reverse.py
+++ num_reverse.py
@@ -1,1 +1,15 @@
+#Reversing Number In Python
+
+#Method One
+ 
+  
+num = int(input("Number: ")) #input from user
+reversed_num = 0
+
+while num != 0:  
+    digit = num % 10
+    reversed_num = reversed_num * 10 + digit
+    num //= 10   
+
+print("Reversed Number is: " , reversed_num )
 
