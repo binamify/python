b61abfb126131ddab66200795e7fa5ad17bf203e
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-08-14 20:50:08 +0545

    Update loop_over_dict.py

--- loop_over_dict.py
+++ loop_over_dict.py
@@ -1,1 +1,18 @@
+#Accessing Python Dictionary Using for loop
+my_dict = {'a':'Python','b':'Javascript','c':'Golang'}
 
+for key, value in my_dict.items():#Accessing both key and value using items()
+	print(key, value)
+
+for key in my_dict:#Accessing both key and value without using items()
+	print(key,my_dict[key])
+
+#Accessing both key and value using iteritems()
+for key, value in my_dict.iteritem():#[doesnt work in python3]
+	print(key, value)
+
+for key in my_dict.keys():#Returning keys explicitly
+    print(key)
+
+for value in my_dict.values():#Returning values explicitly
+    print(value)
