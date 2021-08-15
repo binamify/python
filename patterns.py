f45c098f0b9c9964ba2d02bda84fd8975f698151
Binam Karki <58617203+Explorer720@users.noreply.github.com>
2021-08-15 18:49:27 +0545

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,1 +1,25 @@
 
+# #Patterns in python
+
+# rows = int(input("Enter number of rows: "))
+
+# #Code1
+# for i in range(rows):
+#     for j in range(i+1):
+#         print("*", end="")
+#     print("\n")
+
+# #Code2
+# for i in range(rows):
+#     for j in range(i+1):
+#         print(j+1, end=" ")
+#     print("\n")
+
+#Result
+#	Code1				Code2
+#	*				     1
+#	* *				   1 2
+#	* * *				 1 2 3
+#	* * * *			 1 2 3 4
+#	* * * * *		 1 2 3 4 5
+
